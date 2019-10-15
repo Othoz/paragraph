4ba82444b6e3bce5357c8ee4e8c6c58c405961b5
Pierre-Yves Bourguignon <bourguignon@othoz.com>
2019-10-15 19:15:35 +0200

    Refactor tests

--- othoz/paragraph/tests/test_session.py
+++ othoz/paragraph/tests/test_session.py
@@ -1,39 +1,21 @@
 import pytest
-import attr
-from unittest.mock import MagicMock
 
-from othoz.paragraph.types import Requirement, Variable, Op
+from othoz.paragraph.types import Variable
 from othoz.paragraph.session import traverse_fw, traverse_bw, evaluate, solve_requirements
-
-
-@attr.s
-class TestReq(Requirement):
-    _string = attr.ib(type=str, default="")
-
-    def merge(self, other):
-        self._string += other._string
-        super().merge(other)
-
-
-def mock_op():
-    class MockOp(Op):
-        _run = MagicMock(return_value="Return value")
-        _arg_requirements = MagicMock(return_value=TestReq("Arg requirement"))
-
-    return MockOp()
+from othoz.paragraph.tests.test_types import TestReq, mock_op
 
 
 @pytest.fixture
 def graph():
     graph = lambda: None  # noqa: E731
-    op1 = mock_op()
+    op1 = mock_op("op0")
     graph.input = Variable()
-    output1 = op1(arg=graph.input)
+    output0 = op1(arg=graph.input)
 
-    op2 = mock_op()
-    output2 = op2(arg=graph.input)
+    op2 = mock_op("op1")
+    output1 = op2(arg0=graph.input, arg1=output0)
 
-    graph.output = [output1, output2]
+    graph.output = [output0, output1]
 
     return graph
 
@@ -55,14 +37,14 @@ class TestForwardGenerator:
 class TestBackwardGenerator:
     def test_generated_value_wo_boundary(self, graph):
         items = list(traverse_bw(graph.output))
-        expected = graph.output + [graph.input]
+        expected = [graph.output[1], graph.output[0], graph.input]
 
         assert items == expected
 
     def test_generated_values_with_boundary(self, graph):
         items = list(traverse_bw(graph.output, boundary=[graph.input]))
 
-        assert items == graph.output
+        assert items == [graph.output[1], graph.output[0]]
 
 
 @pytest.mark.parametrize("max_workers", [pytest.param(1, id="Single thread"),
@@ -71,9 +53,11 @@ class TestEvaluation:
     def test_variable_evaluation_is_correct(self, graph, max_workers):
         res = evaluate(graph.output, args={graph.input: "Input value"}, max_workers=max_workers)
 
-        assert res[0] == "Return value"
-        for var in graph.output:
-            var.func.func._run.assert_called_once_with(arg="Input value")
+        assert res[0] == "op0 return value"
+        assert res[1] == "op1 return value"
+
+        graph.output[0].func.func._run.assert_called_once_with(arg="Input value")
+        graph.output[1].func.func._run.assert_called_once_with(arg0="Input value", arg1="op0 return value")
 
     def test_evaluation_is_lazy(self, graph, max_workers):
         res = evaluate([graph.output[0]], args={graph.output[0]: "Input value"}, max_workers=max_workers)
@@ -85,7 +69,7 @@ class TestEvaluation:
 
 class TestRequirementSolving:
     def test_req_update_func_called(self):
-        operation = mock_op()
+        operation = mock_op("")
 
         var = Variable()
         res = operation(a=1, b=var)
--- othoz/paragraph/tests/test_types.py
+++ othoz/paragraph/tests/test_types.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 import attr
 
 from othoz.paragraph.types import op, Op, Variable, Requirement
@@ -13,9 +13,9 @@ class TestReq(Requirement):
         super().merge(other)
 
 
-def mock_op():
+def mock_op(name=""):
     class MockOp(Op):
-        _run = MagicMock(return_value="Return value")
+        _run = MagicMock(return_value="{} return value".format(name))
         _arg_requirements = MagicMock(return_value=TestReq("Arg requirement"))
 
     return MockOp()
@@ -45,22 +45,20 @@ class TestOpDecorator:
 
 class TestOpClass:
     def test_call_invokes_function_if_args_invariable(self):
-        with patch.multiple("othoz.paragraph.types.Op", __abstractmethods__=set(), _run=MagicMock(return_value="Return value")):
-            operation = Op()
-            return_value = operation(a=1, b="b")
+        operation = mock_op("op")
+        return_value = operation(a=1, b="b")
 
-            operation._run.assert_called_once_with(a=1, b="b")
-            assert return_value == "Return value"
+        operation._run.assert_called_once_with(a=1, b="b")
+        assert return_value == "op return value"
 
     def test_call_returns_variable_if_some_arg_variable(self):
-        with patch.multiple("othoz.paragraph.types.Op", __abstractmethods__=set(), _run=MagicMock(return_value="Return value")):
-            operation = Op()
-            input_var = Variable()
-            return_var = operation(a=1, b=input_var)
+        operation = mock_op("op")
+        input_var = Variable()
+        return_var = operation(a=1, b=input_var)
 
-            assert not Op._run.called
-            assert isinstance(return_var, Variable)
-            assert return_var.dependencies == {"b": input_var}
+        assert not operation._run.called
+        assert isinstance(return_var, Variable)
+        assert return_var.dependencies == {"b": input_var}
 
     def test_call_writes_requirements_update_func(self):
         operation = mock_op()
