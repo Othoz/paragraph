85e10cc582d1663c5ca8aeec4cf1f64a8c43910c
Pierre-Yves Bourguignon <bourguignon@othoz.com>
2019-11-04 22:45:22 +0100

    Cleanup after merge

--- othoz/__init__.py
+++
@@ -1,6 +0,0 @@
-# This __init__py has the responsibility to mark the parent folder as the
-# common 'othoz' root namespace that we use for all Python libraries
-# See https://packaging.python.org/guides/packaging-namespace-packages/
-# Note that it appears we are forced to use pkg_resources-style namespace packages
-# because otherwise the conda build complains and fails
-__import__('pkg_resources').declare_namespace(__name__)
